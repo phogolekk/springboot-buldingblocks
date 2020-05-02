67a1c2edb2c2dc17ca410dce4198c5214675df4e
Kgotong Phogoe <phogolekk@gmail.com>
2020-05-03 00:58:47 +0200

    first comment

--- src/main/java/com/stacksimplify/restservices/SpringbootPracticeApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootPracticeApplication.java
@@ -3,6 +3,8 @@ package com.stacksimplify.restservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//welcoming the big boss
 @SpringBootApplication
 public class SpringbootPracticeApplication {
 
